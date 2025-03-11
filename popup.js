901aab00023bf0d0cbeee243db98934ea33eb6a3
shiinedev <shiinecadde17@gmail.com>
2025-03-11 20:11:01 +0300

    added Popup files

---
+++ popup.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        const messageDiv = document.getElementById("status-message");
+        const funMessage = document.querySelector(".fun-message");
+        
+        if (currentTab && currentTab.url.includes("https://x.com/")) {
+            messageDiv.innerText = "Blue badge energy, no subscription needed! 💙";
+            funMessage.innerText = "Don’t tell Elon… but you’re verified now 🤫";
+            
+        } else {
+            messageDiv.innerText = "😢 You`re not in Twitter ! Open Twitter to Get Your Verification.";
+            funMessage.innerText = "Who needs a subscription when you have this? 🤩";
+
+        }
+    });
+});
